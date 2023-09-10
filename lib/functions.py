04a1a0326a5de90febba2045b33b3204acce8f82
Absalom <mrclabsalom@gmail.com>
2023-09-10 10:05:47 +0300

    completed

---
+++ lib/functions.py
@@ -0,0 +1,58 @@
+# stacks
+
+def is_balanced(expression):
+    stack = []
+    brackets = {')': '(', '}': '{', ']': '['}
+
+    for char in expression:
+        if char in '({[':
+            stack.append(char)
+        elif char in ')}]':
+            if not stack or stack.pop() != brackets[char]:
+                return False
+
+    return not stack
+
+expression1 = "([]{})"
+expression2 = "([)]"
+print(is_balanced(expression1))  # Output: True
+print(is_balanced(expression2))  # Output: False
+
+
+# List/Tuples
+def remove_duplicates(sequence):
+    seen = set()
+    result = []
+    
+    for item in sequence:
+        if item not in seen:
+            seen.add(item)
+            result.append(item)
+    
+    return result
+
+input_sequence = [2, 3, 2, 4, 5, 3, 6, 7, 5]
+result = remove_duplicates(input_sequence)
+print(result)  # Output: [2, 3, 4, 5, 6, 7]
+
+
+#Dictionaries
+import string
+
+def word_frequency(sentence):
+    sentence = sentence.lower()
+    sentence = sentence.translate(str.maketrans('', '', string.punctuation))
+    words = sentence.split()
+
+    frequency = {}
+    for word in words:
+        if word in frequency:
+            frequency[word] += 1
+        else:
+            frequency[word] = 1
+
+    return frequency
+
+sentence = "This is a test sentence. This sentence is a test."
+result = word_frequency(sentence)
+print(result)
